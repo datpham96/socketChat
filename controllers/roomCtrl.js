46f8f859c59716abb7a2132a9bbb2705d8f45d4f
dat pham <phamdat22396@gmail.com>
2020-07-01 17:27:50 +0700

    cm

---
+++ controllers/roomCtrl.js
@@ -0,0 +1,59 @@
+const controller = require('./controller');
+const messageFunc = require('../socketProcess/message/myFunc');
+const messageConfig = require('../socketProcess/message/config');
+
+module.exports = class roomCtrl extends controller{
+    constructor(ctx){
+        super(ctx);
+    }
+
+    async addRoom(){
+        
+    }
+
+    async outRoom(){
+
+    }
+
+    async index(){
+        try {
+            let validate = await this.validate(this.getBody(), {
+                'topicId': 'required',
+                'body': 'required',
+                'emailSend': 'required|email',
+                'emailReceive': 'required|array',
+            }, {
+                    'topicId.required': 'topicId không được bỏ trống',
+                    'body.required': 'body không được bỏ trống',
+                    'emailSend.required': 'email người gửi không được bỏ trống',
+                    'emailSend.email': 'email người gửi không hợp lệ',
+                    'emailReceive.required': 'email người nhạn không được bỏ trống',
+                    'emailReceive.array': 'email người nhận không đúng dịnh dạng'
+                });
+            
+            if (validate.fails()) {
+                console.log(validate.messages())
+                return this.response(validate.messages(), 422);
+                
+            }
+    
+            let emailSend = this.getInput('emailSend');
+            let emailReceive = this.getInput('emailReceive')
+            let topicId = this.getInput('topicId');
+            let body = this.getInput('body', '');
+            let articleId = this.getInput('articleId', '')
+            let typeMessage = this.getInput('typeMessage', '')
+            
+            global.messageSocket.to(messageFunc.buildRoomName(topicId)).emit(messageConfig.emitEvent.sendMessage, {emailSend, topicId, body, articleId, typeMessage});
+            // for(let sigleEmailReceive of emailReceive){
+            //     global.messageSocket.to(messageFunc.buildRoomName(sigleEmailReceive)).emit(messageConfig.emitEvent.sendMessage, {emailSend, topicId, body, articleId});
+            // }
+            
+            return this.response({status: true});
+        } catch (error) {
+            console.log(error)
+            return this.response({status: false}, 422);
+        }
+        
+    }
+}
--- routes/api.js
+++ routes/api.js
@@ -2,15 +2,26 @@ const Router = require("koa-router");
 const messageCtrl = require('../controllers/messageCtrl');
 const mdwAddHeaderJson = require('../middleware/apiHeaderResponse');
 const config = require('../config/app.json');
-const auth = require('koa-basic-auth');
+const koaBasicAuth = require('koa-basic-auth');
+
+let apiRouter = new Router({
+    prefix: '/api/v1'
+});
 
 let apiRouter = new Router({
     prefix: '/api/v1'
 });
 
 apiRouter.use(mdwAddHeaderJson);
-apiRouter.use(auth(config.auth));
+apiRouter.use(koaBasicAuth(config.auth));
 
+//sendMessage
 apiRouter.post('/sendMessage', async ctx => { await new messageCtrl(ctx).index() });
+
+//addRoom
+apiRouter.post('/addRom', async ctx => { await new messageCtrl(ctx).addRom() });
+
+//outRoom
+apiRouter.post('/outRoom', async ctx => { await new messageCtrl(ctx).outRoom() });
 
 module.exports = apiRouter;
